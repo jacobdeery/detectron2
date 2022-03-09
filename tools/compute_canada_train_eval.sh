e51fc678d0d17cba62e567fbec25f1edeaaaeb00
Jacob Deery <jacob.deery@gmail.com>
2022-03-09 08:59:33 -0500

    Adjust time limit

--- tools/compute_canada_train_eval.sh
+++ tools/compute_canada_train_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=00:05:00
 #SBATCH --job-name=Panoptic-DeepLab-train
 #SBATCH --account=rrg-swasland
 #SBATCH --cpus-per-task=8             # CPU cores/threads
